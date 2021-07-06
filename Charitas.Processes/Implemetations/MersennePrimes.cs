b5866d924aeb1043a0d6ee69a1eb71ad64cf731b
HashToBeWild <loki@swarm.technology>
2021-07-06 19:58:07 +0200

    Stubbing out structure

--- Charitas.Processes/Implemetations/MersennePrimes.cs
+++ Charitas.Processes/Implemetations/MersennePrimes.cs
@@ -1,8 +1,19 @@
 ﻿using Charitas.Processes.Interfaces;
+using System.Numerics;
+using System.Threading.Tasks;
 
 namespace Charitas.Processes.Implemetations
 {
     public class MersennePrimes : IMersennePrimes
     {
+        public Task FindMersennePrimes(BigInteger start)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> IsMersennePrime(BigInteger candidate)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- Charitas.Processes/Interfaces/IMersennePrimes.cs
+++ Charitas.Processes/Interfaces/IMersennePrimes.cs
@@ -1,6 +1,12 @@
-﻿namespace Charitas.Processes.Interfaces
+﻿using System.Numerics;
+using System.Threading.Tasks;
+
+namespace Charitas.Processes.Interfaces
 {
     public interface IMersennePrimes
     {
+        Task FindMersennePrimes(BigInteger start);
+
+        Task<bool> IsMersennePrime(BigInteger candidate);
     }
 }
